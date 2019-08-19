823a8eef00935868f269b1db7e6fde319095e5d3
myeongjinKim <ab9010@naver.com>
2019-08-19 22:41:03 +0900

    Client Component make it

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,14 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Customer from './components/Customer'
 import './App.css';
+
+const customer = {
+  'name': '김민수',
+  'birthday': '961225',
+  'gender' : '남자',
+  'job' : '대학생'
+}
 
 class App extends Component {
   render() {
     return (
-      <div className="gray-background"> 
-        <img src={logo} lat="logo"/>
-        <h2>Let's Develop Management System!!</h2>
-      </div>
+      <Customer
+        name={customer.name}
+        birthday={customer.birthday}
+        gender={customer.gender}
+        job={customer.job}
+      />
     );
   }
 }
